16d80564653b5b0b6f065cfb710f200cc22e2ab0
鸡好 <rutin_chicken@163.com>
2025-02-09 21:39:08 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,7 +40,7 @@ function togglePlayPause() {
         audio.play();
         audioControl.classList.add('playing');
         audioControl.style.animationPlayState = 'running';
-        showPlaybackStatus('正在播放《town》-remix <br /> Microsoft Windows');
+        showPlaybackStatus('正在播放《town》Microsoft Windows');
     } else {
         audio.pause();
         audioControl.classList.remove('playing');
