502efc3e80cf50f5e230316126e78569d2480c9a
鸡好 <rutin_chicken@163.com>
2025-02-11 10:46:21 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,7 +40,7 @@ function togglePlayPause() {
         audio.play();
         audioControl.classList.add('playing');
         audioControl.style.animationPlayState = 'running';
-        showPlaybackStatus('正在播放《town》Microsoft Windows');
+        showPlaybackStatus('正在播放赶绵羊');
     } else {
         audio.pause();
         audioControl.classList.remove('playing');
